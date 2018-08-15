187e342806eee8b5050189f615e5652636d8c46c
Arief Hilmi <arief.hilmi@gmail.com>
2018-08-15 11:42:17 +0800

    update code

---
+++ check_url.php
@@ -0,0 +1,20 @@
+<?php
+
+	//require_once('vendor/autoload.php');
+	//use Screen\Capture;
+	set_time_limit(0);
+	$http_code = $_GET['http_code'];
+	$host = $_GET['host'];
+	
+	$ch = curl_init($host);  
+	curl_setopt($ch, CURLOPT_TIMEOUT, 60);  
+	curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 60);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); 
+	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+	curl_setopt($ch, CURLOPT_MAXREDIRS, 10);
+	$data = curl_exec($ch);  
+	$response = curl_getinfo($ch);
+
+	echo json_encode($response);
+	
+?>
